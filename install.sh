6df560b0a94de9bd4f53c1452e53379aeca9d95c
Mark Fay <fay.mark@gmail.com>
2016-04-30 22:11:50 -0400

    added plugin that I might try

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 # git submodule add https://github.com/tpope/vim-pathogen.git 
 # git submodule add https://github.com/scrooloose/nerdtree.git
 
+# thinking about this
+# git submodule add https://github.com/garbas/vim-snipmate
+# allows TextMate tab completion templates
+
 mkdir -p ~/.vim/{autoload,bundle}
 
 cp vim-pathogen/autoload/pathogen.vim ~/.vim/autoload/.
