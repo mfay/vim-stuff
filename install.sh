926aa11b5af3480e479f861647c977bc68578bbd
Mark Fay <faymark@gvsu.edu>
2017-07-14 14:16:35 -0400

    added tmux to the mix

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ git clone https://github.com/tpope/vim-pathogen.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/SirVer/ultisnips.git
 git clone https://github.com/honza/vim-snippets
+git clone https://github.com/christoomey/vim-tmux-navigator
 
 mkdir -p ~/.vim/{autoload,bundle}
 
@@ -11,4 +12,5 @@ cp vim-pathogen/autoload/pathogen.vim ~/.vim/autoload/.
 cp -r nerdtree ~/.vim/bundle/.
 cp -r ultisnips ~/.vim/bundle/.
 cp -r vim-snippets ~/.vim/bundle/.
+cp -r vim-tmux-navigator ~/.vim/bundle/.
 cp .vimrc ~/.vimrc
